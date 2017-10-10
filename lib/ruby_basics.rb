d65897c3ff3152fc9289a57564391c8a774c2eb6
biannak6288 <biannak6288@hstat.org>
2017-10-10 23:46:21 +0000

    fix all failures

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+def division(num1,num2)
+    num1/num2
 end
 
-def argue
-  
+def argue(argument)
+    argument
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(pizza="cheese")
+    pizza
 end
 
-def whisper
+def whisper(argument)
+    argument.downcase
     
 end
